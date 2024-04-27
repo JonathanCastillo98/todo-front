7ecadaea473790e0c80c355c520686c09191973c
Jonathan Castillo <jcastillo@kobu.io>
2024-04-26 22:50:41 -0600

    updates

--- src/components/TaskMini/TaskMini.tsx
+++ src/components/TaskMini/TaskMini.tsx
@@ -2,7 +2,8 @@ import './taskMini.css'
 import { LuGoal } from 'react-icons/lu';
 import { MdDone } from 'react-icons/md';
 import { ISubTask } from '../../interfaces/subTask.interface';
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 type Task = {
     title: string,
     description?: string | null
@@ -14,26 +15,43 @@ type Task = {
     updatedAt: string
 }
 type Props = {
-    task: Task
+    task: Task,
+    onRefresh: () => void
 }
 
-const TaskMini = ({ task }: Props) => {
+const TaskMini = ({ task, onRefresh }: Props) => {
+    const [isEditModalOpen, setIsEditModalOpen] = useState<boolean>(false);
+    const onDeleteTask = async (e: React.MouseEvent<HTMLSpanElement>) => {
+        try {
+            e.stopPropagation();
+            if (task.subTasks.length > 0) {
+                const deleteSubTasksPromises = task.subTasks.map(async (subTask) => {
+                    await axios.delete(`http://localhost:8000/api/sub-tasks/delete/${subTask.id}`);
+                });
+                await Promise.all(deleteSubTasksPromises);
+            }
+            await axios.delete(`http://localhost:8000/api/tasks/delete/${task.id}`);
+            onRefresh();
+        } catch (error) {
+            console.error(error);
+        }
+    }
     const startDrag = (e: React.DragEvent<HTMLDivElement>, item: Task) => {
         e.dataTransfer.setData('itemID', item.id);
 
     }
 
     return (
-        <div className="task-mini" draggable onDragStart={(e) => startDrag(e, task)}>
+        <div className="task-mini" draggable onDragStart={(e) => startDrag(e, task)} onClick={() => setIsEditModalOpen(true)}>
             <div className="task-mini_title">{task.title}</div>
             <div className="task-mini_description">{task.description}</div>
             <footer className='task-mini_footer'>
-                <span className='task-mini_footer_delete-btn'>delete</span>
+                <span onClick={onDeleteTask} className='task-mini_footer_delete-btn'>delete</span>
                 {
                     task.subTasks.length > 0 && (
                         <div className="task-mini_footer_sub-tasks-status">
                             <MdDone />
-                            <span>[{task.subTasks.filter(subTask => subTask.status === "COMspanLETED").length}/{task.subTasks.length}]</span>
+                            <span>[{task.subTasks.filter(subTask => subTask.status === "COMLETED").length}/{task.subTasks.length}]</span>
                         </div>
                     )
                 }
